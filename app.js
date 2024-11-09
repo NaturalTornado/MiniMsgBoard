036da49e8c35d0c3894e161545c4595784e18449
Steve Mathewson <steve.mathewson4@gmail.com>
2024-11-09 13:28:52 -0700

    Port -> env variable: process nov9

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     const path = require("path");
     const app = express();
 
-    const PORT = 3000;
+    const PORT = process.env.port || 3000;
 
     const newMessage = require('./newMessage');
 const { title } = require("process");
